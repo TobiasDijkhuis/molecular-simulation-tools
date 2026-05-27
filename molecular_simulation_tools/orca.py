08ffd583a826a45537646f3fc81a8e04d8a53a6e
Tobias Dijkhuis <tobiasdijkhuis@hotmail.com>
2026-05-27 13:53:50 +0200

    Add testing for get_calculator_from_orca_inp

--- molecular_simulation_tools/orca.py
+++ molecular_simulation_tools/orca.py
@@ -2,16 +2,20 @@
 
 from pathlib import Path
 
-from ase.calculators.orca import ORCA
+from ase.calculators.orca import ORCA, OrcaProfile
 
 
-def get_calculator_from_orca_inp(path: str | Path) -> ORCA:
+def get_calculator_from_orca_inp(
+    path: str | Path, profile: OrcaProfile | None = None
+) -> ORCA:
     """Create an ORCA instance from an ``orca.inp`` file.
 
     Parameters
     ----------
     path : str | Path
         Path to ``orca.inp`` file.
+    profile : OrcaProfile | None
+        OrcaProfile with command to use. Default = None (infer from environment)
 
     Returns
     -------
@@ -24,18 +28,22 @@ def get_calculator_from_orca_inp(path: str | Path) -> ORCA:
     with path.open() as file:
         lines = file.readlines()
 
-    calc = _get_calculator_from_orca_inp_lines(lines)
+    calc = _get_calculator_from_orca_inp_lines(lines, profile=profile)
     calc.directory = path.parent
     return calc
 
 
-def get_calculator_from_orca_out(path: str | Path) -> ORCA:
+def get_calculator_from_orca_out(
+    path: str | Path, profile: OrcaProfile | None = None
+) -> ORCA:
     """Create an ORCA instance from an ``orca.out`` file.
 
     Parameters
     ----------
     path : str | Path
         Path to ``orca.out`` file.
+    profile : OrcaProfile | None
+        OrcaProfile with command to use. Default = None (infer from environment)
 
     Returns
     -------
@@ -64,18 +72,24 @@ def get_calculator_from_orca_out(path: str | Path) -> ORCA:
     if start_line_number is None or end_line_number is None:
         raise RuntimeError
 
-    calc = _get_calculator_from_orca_inp_lines(lines[start_line_number:end_line_number])
+    calc = _get_calculator_from_orca_inp_lines(
+        lines[start_line_number:end_line_number], profile=profile
+    )
     calc.directory = path.parent
     return calc
 
 
-def _get_calculator_from_orca_inp_lines(lines: list[str]) -> ORCA:
+def _get_calculator_from_orca_inp_lines(
+    lines: list[str], profile: OrcaProfile | None = None
+) -> ORCA:
     """Get an ORCA instance from some lines.
 
     Parameters
     ----------
     lines : list[str]
         lines of, for example, an ``orca.inp`` file.
+    profile : OrcaProfile | None
+        OrcaProfile with command to use. Default = None (infer from environment)
 
     Returns
     -------
@@ -92,13 +106,13 @@ def _get_calculator_from_orca_inp_lines(lines: list[str]) -> ORCA:
     orcablocks = ""
     simpleinput, charge, mult = None, None, None
     for line in lines:
-        line = line.split(">")[1]
+        line = line.split(">")[-1]
         line = line.split("#")[0]
         line = line.strip()
         if not line:
             continue
         if line.startswith("!"):
-            simpleinput = line
+            simpleinput = line.split("!")[1].strip()
         elif line.startswith("*"):
             split_line = line.split()
             charge = int(split_line[-2])
@@ -107,11 +121,16 @@ def _get_calculator_from_orca_inp_lines(lines: list[str]) -> ORCA:
             break
         else:
             orcablocks = "\n".join((orcablocks, line))
+
     if simpleinput is None or charge is None or mult is None:
         raise RuntimeError
 
     calc = ORCA(
-        orcasimpleinput=simpleinput, orcablocks=orcablocks, charge=charge, mult=mult
+        profile=profile,
+        orcasimpleinput=simpleinput.strip(),
+        orcablocks=orcablocks.strip(),
+        charge=charge,
+        mult=mult,
     )
 
     return calc
---
+++ tests/test_orca.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from ase import Atoms
+from ase.calculators.orca import ORCA, OrcaProfile
+
+from molecular_simulation_tools.orca import get_calculator_from_orca_inp
+
+orca_test_calculator = ORCA(
+    profile=OrcaProfile("echo 'dummy'"),
+    orcasimpleinput="B3LYP def2-tZVPD",
+    charge=0,
+    mult=2,
+    orcablocks="% HFTyp UHF end\n% Guess PModel end",
+)
+test_atoms = Atoms(symbols="H", positions=[[0, 0, 0]])
+
+
+def test_get_calculator_from_orca_inp(tmpdir):
+    dir = Path(tmpdir)
+    orca_test_calculator.directory = dir
+    orca_test_calculator.write_inputfiles(test_atoms, properties=["energy"])
+    calculator = get_calculator_from_orca_inp(
+        dir / "orca.inp", profile=OrcaProfile("echo 'dummy'")
+    )
+    assert calculator.parameters == orca_test_calculator.parameters
+    assert calculator.directory == orca_test_calculator.directory
