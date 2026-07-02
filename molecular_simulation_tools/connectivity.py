b7874b71bbd51e080c8776cf2b1cb82b9bd59e4c
TobiasDijkhuis <tobiasdijkhuis@hotmail.com>
2026-07-02 09:44:47 +0200

    Fix typo

--- molecular_simulation_tools/connectivity.py
+++ molecular_simulation_tools/connectivity.py
@@ -112,7 +112,7 @@ def complete_intact_molecules(
 ) -> np.ndarray:
     """Get the indices of atoms to keep `indices` fully connected.
 
-    Create a neighborlist of the original atoms using :func:`identiy_molecules`,
+    Create a neighborlist of the original atoms using :func:`identify_molecules`,
     and then make sure that any index in `indices` is kept fully connected
     to its neighbors.
 
